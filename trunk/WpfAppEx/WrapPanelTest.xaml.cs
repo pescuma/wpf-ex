7a1b485f90d3ac8954c81279ba171da6c6db1986
pescuma <pescuma@78e6fca4-51be-365d-77c1-6ffb643da855>
2010-11-24 03:21:27 +0000

    Added missing files

---
+++ trunk/WpfAppEx/WrapPanelTest.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppEx
+{
+	/// <summary>
+	/// Interaction logic for WrapPanelTest.xaml
+	/// </summary>
+	public partial class WrapPanelTest : UserControl
+	{
+		public WrapPanelTest()
+		{
+			InitializeComponent();
+		}
+	}
+}
